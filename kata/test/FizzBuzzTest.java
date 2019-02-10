b590edb923a154cc0d055ffb9ab44bb43c1989a9
Sachin <shaldipur79@gmail.com>
2019-02-09 22:13:35 -0500

    Refactored FizzBuzz

--- kata/test/FizzBuzzTest.java
+++ kata/test/FizzBuzzTest.java
@@ -1,27 +1,33 @@
+import org.junit.Before;
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class FizzBuzzTest {
+
+    FizzBuzz fizzBuzz;
+    @Before
+    public void setUp()
+    {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void whenFizzBuzzIsPassedANumberItReturnsThatNumber()
     {
-       FizzBuzz fizzBuzz = new FizzBuzz();
-       assertEquals("1", fizzBuzz.fizz(1));
+        assertEquals("1", fizzBuzz.fizz(1));
     }
 
 
     @Test
     public void whenFizzBuzzIsPassedATwoItReturnsATwo()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("2", fizzBuzz.fizz(2));
+         assertEquals("2", fizzBuzz.fizz(2));
     }
 
     @Test
     public void whenFizzBuzzIsPassedAMultipleOfThreeItReturnsFizz()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("fizz", fizzBuzz.fizz(3));
+         assertEquals("fizz", fizzBuzz.fizz(3));
         assertEquals("fizz", fizzBuzz.fizz(6));
     }
 }
